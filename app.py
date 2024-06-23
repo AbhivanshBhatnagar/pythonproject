19e35ec1b306ccdd5c469fc85c734378ab9d8308
AbhivanshBhatnagar <bhatnagar1102abhivansh@gmail.com>
2024-06-23 16:11:31 +0530

    test

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def connect_db():
         return conn
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
+            print("Something is wrong with your user name or password. Please coorect it.")
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
             print("Database does not exist")
         else:
